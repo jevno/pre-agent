d33d59fb65ff06be8cf9fd72033298fbe0469be1
DirtyLiar <912570126@qq.com>
2019-12-17 14:24:23 +0800

    配置文件解析

---
+++ pre-agent/src/test/java/com/preapm/agent/aop/springaop/Test.java
@@ -0,0 +1,34 @@
+package com.preapm.agent.aop.springaop;
+
+
+import com.preapm.agent.Bootstrap;
+
+import java.lang.reflect.Method;
+import java.util.regex.Pattern;
+
+public class Test {
+
+    public static void main(String[] args) {
+        JdkRegexpMethodPointcut pointcut = new JdkRegexpMethodPointcut();
+        String[] strings = new String[1];
+
+        strings[0] = "com.preapm.agent.*.print.*";
+
+
+        pointcut.setPatterns(strings);
+
+        Pattern compile = Pattern.compile(strings[0]);
+
+        Pattern[] p = new Pattern[1];
+        p[0] = compile;
+        pointcut.setCompiledPatterns(p);
+
+        Method method = Bootstrap.class.getMethods()[1];
+        System.out.println(method.toString());
+
+
+//        System.out.println(pointcut.matchesPattern("com.preapm.agent.Bootstrap.print"));
+        System.out.println(pointcut.matchesPattern(method.toString()));
+
+    }
+}
